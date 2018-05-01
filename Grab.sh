e24d4cae77f6f42132abe82ac732b208265e7897
Ian Dunn <iondune@gmail.com>
2018-05-01 06:13:02 -0700

    Grab url as parameter

--- Grab.sh
+++ Grab.sh
@@ -1,13 +1,18 @@
 #! /usr/bin/env bash
 
 
-url='https://jobs.intel.com/ListJobs/All/Search/jobtitle/software/country/us/city/folsom/state/ca/'
+url="$1"
+
+if [ -z "$url" ]; then
+	echo 'usage: Grab.sh <url>'
+	exit 1
+fi
 
 mkdir -p 'jobs'
 
 for job in $(wget "$url" --quiet --output-document - | grep -Po '/ShowJob/Id/\K[0-9]+') ; do
 
-	if [ ! -f "jobs/$job.html" ] ; then
+	if [ ! -f "jobs/$job.txt" ] ; then
 		echo "new job: $job"
 		xidel --silent "https://jobs.intel.com/ShowJob/Id/$job" --css '.jobdescriptiontbl' > "jobs/$job.txt"
 	fi
