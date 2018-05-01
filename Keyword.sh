657ccfe65248298eb08bc073466cbf5cc919360d
Ian Dunn <iondune@gmail.com>
2018-05-01 06:07:08 -0700

    Print job titles as well

--- Keyword.sh
+++ Keyword.sh
@@ -4,7 +4,9 @@ for match in $(find jobs -type f | xargs grep -il "$1") ; do
 	match=$(basename $match)
 	id="${match%.*}"
 
-	echo "https://jobs.intel.com/ShowJob/Id/$id"
+	echo -n "https://jobs.intel.com/ShowJob/Id/$id"
+	echo -n " - "
+	xidel --silent "https://jobs.intel.com/ShowJob/Id/$id" --css '.jobdescriptiontbl h3'
 
 done
 
