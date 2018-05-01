ca00546db5a828045836c416a680da84ecb98042
Ian Dunn <iondune@gmail.com>
2018-05-01 06:21:20 -0700

    Add job number to keyword match

--- Keyword.sh
+++ Keyword.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env bash
 
-for match in $(find jobs -type f | xargs grep -il "$1") ; do
-	match=$(basename $match)
+for file in $(find jobs -type f | xargs grep -il "$1") ; do
+	match=$(basename $file)
 	id="${match%.*}"
 
 	echo -n "https://jobs.intel.com/ShowJob/Id/$id"
-	echo -n " - "
+	echo -n " - ("
+	grep -oE 'JR[0-9]+' $file | tr -d '\n'
+	echo -n ") "
 	xidel --silent "https://jobs.intel.com/ShowJob/Id/$id" --css '.jobdescriptiontbl h3'
 
 done
