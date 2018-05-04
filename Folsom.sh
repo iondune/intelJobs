ff646652f80cc81c4e719a303d6461860fd8aa32
Ian Dunn <iondune@gmail.com>
2018-05-03 21:02:27 -0700

    Add scripts to download new jobs from OR and Folsom

---
+++ Folsom.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-1
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-2
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-3
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-4
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-5
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-6
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-7
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-8
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-9
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/city/folsom/state/ca/Page-10
+
