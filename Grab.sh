8a3a97ef594e5d7cdcb1cb6c9e9fd69a6df68a7d
Ian Dunn <iondune@gmail.com>
2018-05-03 21:00:45 -0700

    List links and info about new jobs when grabbing

--- Grab.sh
+++ Grab.sh
@@ -13,8 +13,13 @@ mkdir -p 'jobs'
 for job in $(wget "$url" --quiet --output-document - | grep -Po '/ShowJob/Id/\K[0-9]+') ; do
 
 	if [ ! -f "jobs/$job.txt" ] ; then
-		echo "new job: $job"
 		xidel --silent "https://jobs.intel.com/ShowJob/Id/$job" --css '.jobdescriptiontbl' > "jobs/$job.txt"
+
+		echo -n "https://jobs.intel.com/ShowJob/Id/$job"
+		echo -n " - ("
+		grep -oE 'JR[0-9]+' "jobs/$job.txt" | tr -d '\n'
+		echo -n ") "
+		xidel --silent "https://jobs.intel.com/ShowJob/Id/$job" --css '.jobdescriptiontbl h3'
 	fi
 done
 
