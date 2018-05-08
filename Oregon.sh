26641f5da3b7b61d5524f56833b31d3ba7cdc99a
Ian Dunn <iondune@gmail.com>
2018-05-08 13:45:25 -0700

    Fix Oregon script downloading singapore jobs

--- Oregon.sh
+++ Oregon.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 
 
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-1
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-2
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-3
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-4
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-5
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-6
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-7
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-8
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-9
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-10
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-11
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-12
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-13
-Grab.sh https://jobs.intel.com/ListJobs/All/Search/state/or/Page-14
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-1
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-2
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-3
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-4
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-5
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-6
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-7
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-8
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-9
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-10
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-11
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-12
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-13
+Grab.sh https://jobs.intel.com/ListJobs/All/Search/country/us/state/or/Page-14
